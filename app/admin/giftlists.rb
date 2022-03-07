d08f2a3ac2e3858a572488b556316bcc6d0ca696
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-07 20:02:16 +0000

    Whitelist columns in Giftlist for ActiveAdmin

--- app/admin/giftlists.rb
+++ app/admin/giftlists.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Giftlist do
 
+ permit_params :user_id, :friend_name, :gift_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
