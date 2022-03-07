875a0450d89d96debff58ae73b2ac3569cfad950
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-07 20:03:18 +0000

    Generate graphiti resources for stores

---
+++ app/resources/store_resource.rb
@@ -0,0 +1,15 @@
+class StoreResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :store_name, :string
+  attribute :address, :string
+  attribute :phone_number, :string
+  attribute :email, :string
+  attribute :password, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
