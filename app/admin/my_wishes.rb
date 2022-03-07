4c3f302c0d422b9e039f2fa1962d43eaccbca0ff
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-07 20:02:12 +0000

    Whitelist columns in MyWish for ActiveAdmin

--- app/admin/my_wishes.rb
+++ app/admin/my_wishes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MyWish do
 
+ permit_params :user_id, :gift_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
