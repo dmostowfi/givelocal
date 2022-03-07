7d3f217bd6acd71029288d76e09e8268b5ebacf0
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-07 20:01:54 +0000

    Direct association between users and giftlists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :giftlists,
+             :dependent => :destroy
+
   has_many   :wishlists,
              :class_name => "MyWish",
              :dependent => :destroy
