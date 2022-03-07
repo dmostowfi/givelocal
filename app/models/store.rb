45899ea73b492ac533909d7474ad76b7adc09490
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-07 20:01:54 +0000

    Validate presence of stores.address

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,6 +5,8 @@ class Store < ApplicationRecord
 
   # Validations
 
+  validates :address, :presence => true
+
   validates :phone_number, :uniqueness => true
 
   validates :phone_number, :presence => true
