ba030cf4b5c73756718239f1395ee6a67fe04e4b
anonymous anonymous <deanna.mostowfi@kellogg.northwestern.edu>
2022-03-12 20:15:17 +0000

    2nd draft

--- app/models/store.rb
+++ app/models/store.rb
@@ -43,9 +43,4 @@ class Store < ApplicationRecord
   def to_s
     store_name
   end
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
